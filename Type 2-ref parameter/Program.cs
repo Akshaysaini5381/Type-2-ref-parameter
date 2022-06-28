762179b6f8c98daf6c90669a9c28545d07310e8b
Akshaysaini5381 <akshaysaini5381@gmail.com>
2022-06-29 04:04:26 +0530

    Add project files.

---
+++ Type 2-ref parameter/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Type_2_ref_parameter
+{
+    class Program
+    {
+        public void show(ref int x, ref int y)
+        {
+           Console.WriteLine("Hello :"+x + " " + y);
+            int z = x;
+            x = y;
+            y = z;
+            Console.WriteLine(x + " " + y);
+        }
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the  first number :");
+            int a = int.Parse(Console.ReadLine());
+            Console.Write("Enter the  secound number :");
+            int b = int.Parse(Console.ReadLine());
+            Console.WriteLine("hi :"+a + " " + b);
+            Program obj = new Program();
+            obj.show(ref a,ref b);
+            Console.WriteLine("Last: "+a + " " + b);
+
+        }
+    }
+}
